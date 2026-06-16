504255aef1a5a0495e0f44bf826802e52b69b8c8
Sam <samhostad@hotmail.com>
2026-06-17 02:49:10 +1200

    ADD: Started a script to import raw text quotes to db

--- Server/Server.js
+++ Server/Server.js
@@ -5,23 +5,39 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173'
+    origin: 'http://localhost:5173'
 }));
 
 // Example API endpoint fetching users from PostgreSQL
 app.get('/api/users', async (req, res) => {
     console.log("running query")
-  try {
-    const result = await db.query('SELECT * FROM users ORDER BY id ASC');
-    res.status(209).json(result.rows);
-    console.log(res)
-  } catch (error) {
-    console.error('Database connection error:', error.stack);
-    res.status(500).json({ error: 'Internal server error' });
-  }
+    try {
+        const result = await db.query('SELECT * FROM users ORDER BY id ASC');
+        res.status(209).json(result.rows);
+        console.log(res)
+    } catch (error) {
+        console.error('Database connection error:', error.stack);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
+
+app.post('/api/SignUp', async (req, res) => {
+    console.log("starting Signup")
+    const  {name,password} = req.body
+    console.log(name)
+    console.log(password)
+    try {
+        const result = await db.query(`insert into users (name,password) 
+            values 
+            ($1, $2); `,[name,password])
+        
+        result.status(201).json({messgae: "Great Success"})
+    }catch (err){
+        res.status(500).json({message: "Error during SignUp"})
+    }
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running smoothly on port ${PORT}`);
+    console.log(`Server is running smoothly on port ${PORT}`);
 });
