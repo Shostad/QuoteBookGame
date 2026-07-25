940b1d64f2519ad64452f5f0f118947589325a55
HOSSA533 <samhostad@hotmail.com>
2026-07-26 00:59:38 +1200

    CHANGE: Renamed DB files to make more sense

--- Server/POSTGRESScripts/QuoteBookViews.sql
+++ Server/POSTGRESScripts/QuoteBookViews.sql
@@ -1,4 +1,4 @@
-drop procedure if exists addquote;
+drop procedure if exists addQuote;
 drop view if exists total_quotes;
 drop view if exists stats_by_person;
 drop view if exists quotes_by_month;
@@ -19,6 +19,20 @@ BEGIN
 END 
 $$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE procedure addQuote(lines varchar(255)[], people varchar(255)[], user_id int)
+as $$
+DECLARE
+    current_name varchar(255);
+	current_quote_id int = 1000;
+	line_count int = 1;
+BEGIN
+	insert into quote_head (date,creator_id) values (CAST('01/01/28' as DATE),user_id) returning quote_id into current_quote_id;
+    FOR current_name IN SELECT unnest(people) LOOP
+		insert into quote_line (line_num,person_id,quote_id,text) values (line_count,(select person_id from person where "name" = current_name and created_by = 1),current_quote_id,lines[line_count]);
+		line_count = line_count + 1;
+    END LOOP;
+END 
+$$ LANGUAGE plpgsql;
 
 create or replace view total_quotes as 
 	select qh.quote_id, qh.creator_id from (quote_head qh
@@ -26,7 +40,7 @@ create or replace view total_quotes as
 
 
 create or replace view stats_by_person as 
-	select p.name, temp.count 
+	select p.name, temp.count, p.created_by
 		from ( select ql.person_id,count(ql.person_id) as count 
 			from quote_line ql group by ql.person_id) as temp 
 			join person p on (p.person_id = temp.person_id)
